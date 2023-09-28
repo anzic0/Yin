b081e3f61afd542b94a257720d3c5f777e0206b9
Anton Zickler <anton.zickler@ae.mpg.de>
2023-09-28 16:22:49 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name="Yin",
+    version='0.3',
+    description="Fast Python implementation of the Yin algorithm: a fundamental frequency estimator",
+    author=["Patrice Guyot"],
+    pymodules=["gcmi"],
+    install_requires=[numpy, scipy, matplotlib],
+)
+
